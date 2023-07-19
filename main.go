74aa3ef6fa07e4b1f4ec7024da73bbaa180757fa
Cevdet Ahmet TURAN <ahmetcevdetturan04@hotmail.com>
2023-07-20 02:13:24 +0300

    1.1

--- main.go
+++ main.go
@@ -1,24 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"golesson/heaps"
-)
+import "golesson/heaps"
 
 func main() {
 	//fmt.Println("afsd")
-	m := heaps.MaxHeap{}
-	fmt.Println(m)
-
-	buildHeap := []int{10, 20, 30, 5, 7, 9, 11, 13, 15, 17}
-	for _, v := range buildHeap {
-		m.Insert(v)
-		fmt.Println(m)
-	}
-
-	for i := 0; i < 5; i++ {
-		m.Extract()
-		fmt.Println(m)
-	}
-
+	heaps.Demo1()
 }
