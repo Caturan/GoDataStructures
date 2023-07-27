57c753edbc59c8fe4bb8240793edf4e23c52644a
Cevdet Ahmet TURAN <ahmetcevdetturan04@hotmail.com>
2023-07-27 17:16:52 +0300

    2.1

--- linkedlists/linkedlist1.go
+++ linkedlists/linkedlist1.go
@@ -21,16 +21,65 @@ func (l *linkedList) prepend(n *node) { // The receiver is going to be a linked 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// And in most cases we'll see a pointer to receive and more often that a value receiver.
 }
 
+// In there we will not use the pointer we print the value
+func (l linkedList) printListData() {
+	toPrint := l.head
+	for l.length != 0 {
+		fmt.Printf("%d ", toPrint.data)
+		toPrint = toPrint.next
+		l.length--
+	}
+	fmt.Printf("\n")
+}
+
+// Delete func
+func (l *linkedList) deleteWithValue(value int) {
+	if l.length == 0 {
+		return
+	}
+
+	if l.head.data == value { // When delete value is the head
+		l.head = l.head.next
+		l.length--
+		return
+	}
+	previousToDelete := l.head
+	for previousToDelete.next.data != value {
+		if previousToDelete.next.next == nil { // If the value is not in the list
+			return
+		}
+		previousToDelete = previousToDelete.next
+	}
+	previousToDelete.next = previousToDelete.next.next
+	l.length--
+}
+
 func Demo1() {
 	myList := linkedList{}
-	node1 := &node{data: 48} // We need to pass in a poniter (&) that is what this for it needs to be a pointer
+	node1 := &node{data: 48} // We need to pass in a pointer (&) that is what this for it needs to be a pointer
 	node2 := &node{data: 18}
 	node3 := &node{data: 16}
+	node4 := &node{data: 19}
+	node5 := &node{data: 27}
+	node6 := &node{data: 71}
 
 	myList.prepend(node1)
 	myList.prepend(node2)
 	myList.prepend(node3)
+	myList.prepend(node4)
+	myList.prepend(node5)
+	myList.prepend(node6)
 
-	fmt.Println(myList)
-	fmt.Println(myList.length)
+	//fmt.Println(myList)
+	fmt.Println("List length:", myList.length)
+	myList.printListData()
+	myList.deleteWithValue(16)
+	myList.printListData()
+
+	myList.deleteWithValue(99)
+	myList.deleteWithValue(71)
+	myList.printListData()
+
+	emptyList := linkedList{}
+	emptyList.deleteWithValue(10)
 }
